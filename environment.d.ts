b1eda2d0b128886fd11f812ffe60a66bb9408fba
Alvaro Somoza <somoza.alvaro@gmail.com>
2022-07-16 20:21:11 -0400

    typedef for env

--- environment.d.ts
+++ environment.d.ts
@@ -1,11 +1,15 @@
 declare global {
+  // eslint-disable-next-line no-unused-vars
   namespace NodeJS {
+    // eslint-disable-next-line no-unused-vars
     interface ProcessEnv {
       PORT: string;
       SALT_ROUNDS: string;
       SESSION_SECRET: string;
+      FFMPEG: string;
+      STORAGE: string;
     }
   }
 }
 
-export {}
+export {};
